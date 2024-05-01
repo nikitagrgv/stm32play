364ee57a70a55eb1fb5e8c2239bff50e12a8cd4c
Nikita Grigorev <pettyaelse@gmail.com>
2024-05-02 00:35:22 +0700

    a

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -13,16 +13,36 @@ void switch_led()
 extern "C"
 {
     uint32_t total_msec = 0;
+    size_t last_msec = 0;
     void SysTick_Handler()
     {
         ++total_msec;
+    }
+
+    void USART1_IRQHandler(void)
+    {
+        if (USART1->SR & USART_SR_TC)
+        {
+            volatile uint32_t a = total_msec - last_msec;
+            volatile uint32_t b = a + 1;
+            USART1->SR &= ~USART_SR_TC;
+        }
+        else if (USART1->SR & USART_SR_RXNE)
+        {
+            switch_led();
+            volatile uint8_t data = USART1->DR;
+            volatile char c = data;
+        }
     }
 }
 
 enum class GPIOMode
 {
     FloatingInput = 0b0100,
-    GeneralOpenDrain50MHz = 0b0101,
+    GeneralPushPull50MHz = 0b0011,
+    GeneralOpenDrain50MHz = 0b0111,
+    AlternatePushPull50MHz = 0b1011,
+    AlternateOpenDrain50MHz = 0b1111,
 };
 
 constexpr uint32_t getGPIOMask(GPIOMode mode, int pos)
@@ -43,8 +63,8 @@ void setPinMode(GPIO_TypeDef *port, int pin, GPIOMode mode)
     const int is_high = pin >= 8;
     const int pos = pin % 8;
     auto &reg = is_high ? port->CRH : port->CRL;
-    const uint32_t mask = getGPIOMask(mode, pos);
     const uint32_t clear_mask = getGPIOClearMask(pos);
+    const uint32_t mask = getGPIOMask(mode, pos);
     reg = (reg & ~clear_mask) | mask;
 }
 
@@ -56,18 +76,36 @@ int main()
     // C13 open drain
     setPinMode(GPIOC, 13, GPIOMode::GeneralOpenDrain50MHz);
 
+    setPinMode(GPIOA, 9, GPIOMode::AlternateOpenDrain50MHz); // USART1 TX
+    setPinMode(GPIOA, 10, GPIOMode::FloatingInput);         // USART1 RX
+
     // SysTick
     SysTick->LOAD = 8'000 - 1;
     SysTick->VAL = 0;
     SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
 
-    size_t last_msec = total_msec;
+    // -------------------------------------------------------------------------
+    // USART1 - for Modbus. 9600 baudrate
+    // -------------------------------------------------------------------------
+    USART1->BRR = 833; // USARTDIV = 8e6 / 9600 = 833
+    USART1->CR1 = USART_CR1_UE | USART_CR1_TE | USART_CR1_RE | USART_CR1_RXNEIE | USART_CR1_TCIE;
+
+    NVIC_EnableIRQ(USART1_IRQn);
+    __enable_irq(); // enable interrupts
+
+    last_msec = total_msec;
     while (true)
     {
         if (total_msec - last_msec >= 500)
         {
-            last_msec = total_msec;
             switch_led();
+            for (int i =0; i < 256; ++i)
+            {
+                while (!(USART1->SR & USART_SR_TXE))
+                {}
+                last_msec = total_msec;
+                USART1->DR = i;
+            }
         }
     }
 }
