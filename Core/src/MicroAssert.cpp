09a827e999d3346a8061ce807187842c17d190d6
Nikita Grigorev <pettyaelse@gmail.com>
2024-12-21 21:41:37 +0700

    MICRO_ASSERT

---
+++ Core/src/MicroAssert.cpp
@@ -0,0 +1,7 @@
+#include "MicroAssert.h"
+
+void micro_assert_func(const char *file, int line, const char *func, const char *expr)
+{
+    while (true)
+    {}
+}
---
+++ Core/src/MicroAssert.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cmsis_compiler.h>
+
+#ifdef NDEBUG
+    #define MICRO_ASSERT(__e) ((void)0)
+#else
+    #define MICRO_ASSERT(__e)                                                                      \
+        ((__e) ? (void)0 : micro_assert_func(__FILE__, __LINE__, __PRETTY_FUNCTION__, #__e))
+#endif
+
+void micro_assert_func(const char *file, int line, const char *func, const char *expr) __NO_RETURN;
