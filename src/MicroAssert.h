e68ad243a6a1136168a7113dc1a17fb00935e190
Nikita Grigorev <pettyaelse@gmail.com>
2024-12-21 22:55:51 +0700

    w

--- src/MicroAssert.h
+++ src/MicroAssert.h
@@ -6,7 +6,7 @@
     #define MICRO_ASSERT(__e) ((void)0)
 #else
     #define MICRO_ASSERT(__e)                                                                      \
-        ((__e) ? (void)0 : micro_assert_func(__FILE__, __LINE__, __PRETTY_FUNCTION__, #__e))
+        ((__e) ? (void)0 : micro_assert_failed(__FILE__, __LINE__, __PRETTY_FUNCTION__, #__e))
 #endif
 
 void micro_assert_failed(const char *file, int line, const char *func, const char *expr) __NO_RETURN;
