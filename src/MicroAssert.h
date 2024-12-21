65510bb0ea6d33d2443682081d13117921104593
Nikita Grigorev <pettyaelse@gmail.com>
2024-12-21 22:53:05 +0700

    w

--- src/MicroAssert.h
+++ src/MicroAssert.h
@@ -9,4 +9,4 @@
         ((__e) ? (void)0 : micro_assert_func(__FILE__, __LINE__, __PRETTY_FUNCTION__, #__e))
 #endif
 
-void micro_assert_func(const char *file, int line, const char *func, const char *expr) __NO_RETURN;
+void micro_assert_failed(const char *file, int line, const char *func, const char *expr) __NO_RETURN;
