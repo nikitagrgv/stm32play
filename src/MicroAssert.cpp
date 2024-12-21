18ad7beea7acc9fc290d3023b1ab38388ba42271
Nikita Grigorev <pettyaelse@gmail.com>
2024-12-21 22:56:09 +0700

    fix build

--- src/MicroAssert.cpp
+++ src/MicroAssert.cpp
@@ -1,6 +1,6 @@
 #include "MicroAssert.h"
 
- void micro_assert_failed(const char *file, int line, const char *func, const char *expr)
+void micro_assert_failed(const char *file, int line, const char *func, const char *expr)
 {
     __disable_irq(); // disable interrupts
     while (true)
