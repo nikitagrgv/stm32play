73f5c9c2a4705f06ce373a8d2b1dd47baf1eecf6
Nikita Grigorev <pettyaelse@gmail.com>
2024-12-21 22:53:40 +0700

    Disable interrupts if assert failed

--- src/MicroAssert.cpp
+++ src/MicroAssert.cpp
@@ -1,7 +1,8 @@
 #include "MicroAssert.h"
 
-void micro_assert_func(const char *file, int line, const char *func, const char *expr)
+ void micro_assert_failed(const char *file, int line, const char *func, const char *expr)
 {
+    __disable_irq(); // disable interrupts
     while (true)
     {}
 }
