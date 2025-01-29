fbacd9b02c14a0866ebd2c1887b0734cdb54f3e4
Nikita Grigorev <pettyaelse@gmail.com>
2025-01-29 22:19:05 +0700

    ww

--- src/core/Globals.cpp
+++ src/core/Globals.cpp
@@ -1,3 +1,5 @@
 #include "Globals.h"
 
+volatile uint32_t glob::SYS_FREQUENCY = 8'000'000;
+
 volatile uint32_t glob::total_msec = 0;
--- src/core/Globals.h
+++ src/core/Globals.h
@@ -5,10 +5,7 @@
 namespace glob
 {
 
-// TODO#!
-// TODO!
-
-constexpr uint32_t SYS_FREQUENCY = 72'000'000;
+extern volatile uint32_t SYS_FREQUENCY;
 
 extern volatile uint32_t total_msec;
 
