e7a9890859d593934c8bd2e583338528c14713f6
Nikita Grigorev <pettyaelse@gmail.com>
2025-02-10 17:58:47 +0700

    w

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,7 @@ int main()
     constexpr Pin usart_tx_pin{GPIOPort::A, 9};
     constexpr Pin usart_rx_pin{GPIOPort::A, 10};
 
-    constexpr Pin i2c_sda_pin{GPIOPort::B, 7};
+    constexpr Pin i2c_sda_pin{GPIOPort::B, 9};
     constexpr Pin i2c_scl_pin{GPIOPort::B, 8};
 
     gpio::configureOutput(led_pin, gpio::OutputMode::OpenDrain, gpio::OutputSpeed::High);
@@ -97,11 +97,43 @@ int main()
     TIM_TypeDef *dht_timer = TIM2;
     command_executor.addCommand(std::make_unique<DHT11Command>(dht_pin, dht_timer));
 
+    struct I2CCommand : public ICommand
+    {
+        const char *name() override { return "go"; }
+        bool execute(const char *args) override
+        {
+            io::printSync("gooo!\n");
+
+
+            I2C1->CR1 &= ~I2C_CR1_PE;  // Disable I2C before configuring
+            I2C1->CR2 = 42;           // Set PCLK frequency in MHz
+            I2C1->CCR = 210;          // Set clock control register for 100 kHz
+            I2C1->TRISE = 43;         // Set maximum rise time
+            I2C1->CR1 |= I2C_CR1_PE;   // Re-enable I2C
+
+            // Send START condition
+            I2C1->CR1 |= I2C_CR1_START;
+            // Wait until START is cleared by hardware
+            while (I2C1->SR1 & I2C_SR1_SB)
+            {}
+            // Send slave address (e.g., 0x50 for write)
+            I2C1->DR = (0x50 << 1) | 0; // Slave address + write bit
+
+            return true;
+        }
+    };
+    command_executor.addCommand(std::make_unique<I2CCommand>());
+
     gpio::setPinOutput(led_pin, false);
 
     irq::enableInterrupts();
 
     io::printSyncFmt("--- Device is ready ---\n");
+
+    // I2C1->CR2 |= (42 << I2C_CR2_FREQ_Pos); // 42Mhz APB1 clock
+    // I2C1->CCR = 210;
+    // I2C1->TRISE = 43;
+    // I2C1->CR1 |= I2C_CR1_PE;
 
     while (true)
     {
