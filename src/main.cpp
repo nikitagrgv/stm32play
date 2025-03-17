07aaf68e0275163413d049897f33c6b4a2504c22
Nikita Grigorev <pettyaelse@gmail.com>
2025-03-18 02:58:16 +0700

    w

--- src/main.cpp
+++ src/main.cpp
@@ -144,6 +144,14 @@ bool check_sht31(float &temperature, float &humidity)
     valid &= utils::crc8(data, 2, crc8_poly, crc8_init) == data[2];
     valid &= utils::crc8(data + 3, 2, crc8_poly, crc8_init) == data[5];
 
+    if (valid)
+    {
+        constexpr float divisor = 2 << 16 - 1;
+        const uint16_t temp = (uint16_t)data[0] << 8 | data[1];
+        const uint16_t hum = (uint16_t)data[3] << 8 | data[4];
+        humidity = 100.0f * (float)hum / divisor;
+        temperature = -45.0f + 175.0f * (float)temp / divisor;
+    }
 
     return valid;
 }
