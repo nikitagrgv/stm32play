a6fba9ecfdbec28367e62a6393f575568b486332
Nikita Grigorev <pettyaelse@gmail.com>
2025-03-18 02:43:26 +0700

    w

---
+++ src/utils/CRC.cpp
@@ -0,0 +1,25 @@
+#include "CRC.h"
+
+uint8_t utils::crc8(const uint8_t *data, int len, uint8_t polynomial, uint8_t initValue)
+{
+    uint8_t crc = initValue;
+
+    for (int b = 0; b < len; ++b)
+    {
+        const uint8_t byte = data[b];
+
+        crc ^= byte;
+        for (int i = 0; i < 8; ++i)
+        {
+            if (crc & 0x80)
+            {
+                crc = (crc << 1) ^ polynomial;
+            }
+            else
+            {
+                crc <<= 1;
+            }
+        }
+    }
+    return crc;
+}
---
+++ src/utils/CRC.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+namespace utils
+{
+
+uint8_t crc8(const uint8_t *data, int len, uint8_t polynomial, uint8_t initValue);
+
+} // namespace utils
