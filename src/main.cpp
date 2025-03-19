cd0b67e530107c97c0ff0129c7f82045a487e8a3
Nikita Grigorev <pettyaelse@gmail.com>
2025-03-20 03:21:31 +0700

    w

--- src/main.cpp
+++ src/main.cpp
@@ -207,19 +207,33 @@ enum class RSMode
 
 constexpr int FAST_DELAY_US = 100;
 
-bool runLcdCommand(I2C_TypeDef *i2c, uint8_t address, uint8_t data, RWMode rw, RSMode rs, bool backlight = true)
+void triggerLcd(I2C_TypeDef *i2c, uint8_t address, uint8_t data)
+{
+    utils::sleepUsec(TIM2, FAST_DELAY_US);
+
+    constexpr uint8_t enable_mask = 1 << 5;
+    constexpr uint8_t enable_clear_mask = ~enable_mask;
+
+    uint8_t transmit_data = data & enable_mask;
+    masterTransmit(i2c, address, &transmit_data, 1);
+
+    utils::sleepUsec(TIM2, FAST_DELAY_US);
+
+    transmit_data = data & enable_clear_mask;
+    masterTransmit(i2c, address, &transmit_data, 1);
+}
+
+void runLcdCommand(I2C_TypeDef *i2c, uint8_t address, uint8_t data, RWMode rw, RSMode rs, bool backlight = true)
 {
     uint8_t base_mask = 0;
     base_mask |= (uint8_t)rw << 6;
     base_mask |= (uint8_t)rs << 7;
     base_mask |= (uint8_t)backlight << 4;
 
-    constexpr uint8_t enable_mask = 1 << 5;
 
     const uint8_t data_high = data >> 4;
     const uint8_t data_low = data & 0x0F;
 
-    utils::sleepUsec(TIM2, FAST_DELAY_US);
 
     uint8_t transmit_data = base_mask | data_high | enable_mask;
     masterTransmit(i2c, address, &transmit_data, 1);
@@ -228,8 +242,6 @@ bool runLcdCommand(I2C_TypeDef *i2c, uint8_t address, uint8_t data, RWMode rw, R
 
     transmit_data = base_mask | data_low;
     masterTransmit(i2c, address, &transmit_data, 1);
-
-    return true;
 }
 
 bool runLcd(I2C_TypeDef *i2c)
