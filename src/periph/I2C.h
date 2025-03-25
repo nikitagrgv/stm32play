b508f606492f0fb21ce5bcda958499bd6fd87933
Nikita Grigorev <pettyaelse@gmail.com>
2025-03-25 17:04:56 +0700

    w

--- src/periph/I2C.h
+++ src/periph/I2C.h
@@ -7,12 +7,12 @@ namespace i2c
 
 constexpr uint32_t DEFAULT_SPEED = 100'000;
 
-void setupI2C(I2C i2c, uint32_t speed = DEFAULT_SPEED);
+bool setupI2C(I2C i2c, uint32_t speed = DEFAULT_SPEED);
 
-void masterTransmitBlocking(I2C i2c, uint8_t address, const uint8_t *buf, uint32_t num_bytes, int timeout_ms = -1);
-void masterReceiveBlocking(I2C i2c, uint8_t address, uint8_t *buf, uint32_t num_bytes, int timeout_ms = -1);
+bool masterTransmitBlocking(I2C i2c, uint8_t address, const uint8_t *buf, uint32_t num_bytes, int timeout_ms = -1);
+bool masterReceiveBlocking(I2C i2c, uint8_t address, uint8_t *buf, uint32_t num_bytes, int timeout_ms = -1);
 
-void masterTransmitBlocking(I2C i2c, uint8_t address, uint8_t value, int timeout_ms = -1);
-void masterReceiveBlocking(I2C i2c, uint8_t address, uint8_t &value, int timeout_ms = -1);
+bool masterTransmitBlocking(I2C i2c, uint8_t address, uint8_t value, int timeout_ms = -1);
+bool masterReceiveBlocking(I2C i2c, uint8_t address, uint8_t &value, int timeout_ms = -1);
 
 } // namespace i2c
