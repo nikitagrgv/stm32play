9df3896fa2a658c124b7686736d9234bb3ca564e
Nikita Grigorev <pettyaelse@gmail.com>
2025-03-25 17:35:00 +0700

    w

--- src/commands/SHT31Command.cpp
+++ src/commands/SHT31Command.cpp
@@ -1,37 +1,34 @@
-#include "DHT11Command.h"
+#include "SHT31Command.h"
 
 #include "Print.h"
-#include "drivers/DHT11Driver.h"
+#include "drivers/SHT31Driver.h"
 #include "periph/PeriphBase.h"
 
-#include "DeviceCMSIS.h"
-
-DHT11Command::DHT11Command(const Pin &pin, TIM_TypeDef *timer)
-    : pin_(pin)
-    , timer_(timer)
+SHT31Command::SHT31Command(I2C i2c)
+    : i2c_(i2c)
 {}
 
-bool DHT11Command::execute(const char *args)
+bool SHT31Command::execute(const char *args)
 {
-    DHT11Driver dht11{pin_, timer_};
+    SHT31Driver sht31{i2c_};
 
     float temperature = 0.0f;
     float humidity = 0.0f;
-    const DHT11Driver::ErrorCode code = dht11.run(temperature, humidity);
+    const SHT31Driver::ErrorCode code = sht31.run(temperature, humidity);
 
-    if (code == DHT11Driver::ErrorCode::Timeout)
+    if (code == SHT31Driver::ErrorCode::Timeout)
     {
         io::printSyncFmt("DHT11: Timeout\n");
         return true;
     }
 
-    if (code == DHT11Driver::ErrorCode::InvalidChecksum)
+    if (code == SHT31Driver::ErrorCode::InvalidChecksum)
     {
         io::printSyncFmt("DHT11: Invalid checksum\n");
         return true;
     }
 
-    if (code != DHT11Driver::ErrorCode::Success)
+    if (code != SHT31Driver::ErrorCode::Success)
     {
         io::printSyncFmt("DHT11: Unexpected error\n");
         return true;
--- src/commands/SHT31Command.h
+++ src/commands/SHT31Command.h
@@ -6,11 +6,11 @@
 class SHT31Command final : public ICommand
 {
 public:
-    SHT31Command(I2C i2c);
+    explicit SHT31Command(I2C i2c);
 
     const char *name() override { return "sht31"; }
     bool execute(const char *args) override;
 
 private:
-    I2C i2c;
+    I2C i2c_;
 };
