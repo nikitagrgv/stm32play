8e1a03d9aa79286787ae1e35d84fe5d9451d98f9
Nikita Grigorev <pettyaelse@gmail.com>
2025-03-25 17:55:16 +0700

    w

--- src/drivers/LCD1602Driver.h
+++ src/drivers/LCD1602Driver.h
@@ -1,4 +1,13 @@
 #pragma once
 
 #include "DeviceCMSIS.h"
+#include "periph/PeriphBase.h"
 
+class LCD1602Driver
+{
+public:
+    LCD1602Driver(I2C i2c, TIM_TypeDef *timer);
+
+private:
+    I2C i2c_;
+};
