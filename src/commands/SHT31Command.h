a3e168ba727762bbaefbea5728595303c93ad06c
Nikita Grigorev <pettyaelse@gmail.com>
2025-03-25 17:33:09 +0700

    w

--- src/commands/SHT31Command.h
+++ src/commands/SHT31Command.h
@@ -8,12 +8,11 @@
 class SHT31Command final : public ICommand
 {
 public:
-    SHT31Command(const Pin &pin, TIM_TypeDef *timer);
+    SHT31Command(I2C i2c);
 
     const char *name() override { return "dht11"; }
     bool execute(const char *args) override;
 
 private:
-    Pin pin_;
-    TIM_TypeDef *timer_{};
+    I2C i2c;
 };
