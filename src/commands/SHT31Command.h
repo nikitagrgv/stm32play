7eb77c4a50ca55313e2cb71e7375fd05a85eb2ec
Nikita Grigorev <pettyaelse@gmail.com>
2025-03-25 17:33:25 +0700

    w

--- src/commands/SHT31Command.h
+++ src/commands/SHT31Command.h
@@ -3,14 +3,12 @@
 #include "CommandExecutor.h"
 #include "periph/PeriphBase.h"
 
-#include "DeviceCMSIS.h"
-
 class SHT31Command final : public ICommand
 {
 public:
     SHT31Command(I2C i2c);
 
-    const char *name() override { return "dht11"; }
+    const char *name() override { return "sht31"; }
     bool execute(const char *args) override;
 
 private:
