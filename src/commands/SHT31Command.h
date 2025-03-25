015cba633734d6d62c445e58dbf4dd57ffd23744
Nikita Grigorev <pettyaelse@gmail.com>
2025-03-25 17:32:35 +0700

    w

---
+++ src/commands/SHT31Command.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CommandExecutor.h"
+#include "periph/PeriphBase.h"
+
+#include "DeviceCMSIS.h"
+
+class DHT11Command final : public ICommand
+{
+public:
+    DHT11Command(const Pin &pin, TIM_TypeDef *timer);
+
+    const char *name() override { return "dht11"; }
+    bool execute(const char *args) override;
+
+private:
+    Pin pin_;
+    TIM_TypeDef *timer_{};
+};
