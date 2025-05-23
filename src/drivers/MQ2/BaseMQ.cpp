b91181f317234bc716edada004dc840472685ed8
Nikita Grigorev <pettyaelse@gmail.com>
2025-05-24 01:20:07 +0700

    w

--- src/drivers/MQ2/BaseMQ.cpp
+++ src/drivers/MQ2/BaseMQ.cpp
@@ -1,13 +1,3 @@
-/****************************************************************************/
-//  Function:       cpp file for BaseMQ
-//  Hardware:       MQ2, MQ3, MQ4, MQ5, MQ6, MQ7, MQ8, MQ9, MQ135
-//  Arduino IDE:    Arduino 1.8.3
-//  Author:         Igor Dementiev
-//  Date:           Jan 19, 2018
-//  Version:        v1.1
-//  by www.amperka.ru
-/****************************************************************************/
-
 #include "BaseMQ.h"
 
 BaseMQ::BaseMQ(uint8_t pin) {
